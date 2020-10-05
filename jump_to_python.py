c6e0ab1aa2adb1a65eed069ffbb65975c263413b
deepdiver <time0909@g.skku.edu>
2020-10-05 17:22:13 +0900

    add taxi ex

---
+++ jump_to_python.py
@@ -0,0 +1,14 @@
+#파이썬 에디터 사용 연습 (1)
+money = 2000
+if money >= 3000 :
+    print("택시를 타고 가라")
+else:
+    print("걸어 가라")
+
+#파이썬 에디터 사용 연습 (2)
+money = 2000
+card = True
+if money >= 3000 or card:
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")

fc020e1cfce85659a4f2dc1d9149633090215fc7
deepdiver <time0909@g.skku.edu>
2020-10-05 16:45:24 +0900

    add new practice file of jump to python

--- io(2).py
+++ io(2).py
@@ -1,3 +1,13 @@
-temperature= float(input("현재 온도를 입력하여 주세요"))
-print(temperature)
-print("Hi", "Way to go!")
+# temperature= float(input("현재 온도를 입력하여 주세요:"))
+# print(temperature)
+# print("Hi", "Way to go!")
+
+# 1회차 숙제
+# Basic
+c_tem = float(input("섭씨 온도를 입력해주세요:"))
+f_tem = c_tem *9/5 +32
+print("화씨 온도는",f_tem,"입니다.")
+
+# 포맷팅
+print('화씨 온도는{}입니다'.format(float(input('섭씨 온도를 입력해주세요:'))*9/5+32))
+

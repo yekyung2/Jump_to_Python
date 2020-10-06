030adf591c9c899b0b3440b9bbf9fb4369184ca7
deepdiver <time0909@g.skku.edu>
2020-10-06 19:01:02 +0900

    practice class

--- class.py
+++ class.py
@@ -1,9 +1,9 @@
 #클래스 구조 만들기
-class FourCal:
-    pass
+# class FourCal:
+#     pass
 
-a = FourCal()
-print(type(a))
+# a = FourCal()
+# print(type(a))
 
 #객체에 숫자 지정할 수 있게 만들기 a.setdata(4,2)
 #클래스 안에서 구현된 함수는 '메서드(Method)'라고 부른다. 
@@ -11,6 +11,9 @@ class FourCal :
     def setdata(self, first, second):
         self.first = first
         self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
 
 a = FourCal()
 a.setdata(4,2)
@@ -23,3 +26,31 @@ print(b.first)
 
 print(id(a.first))
 print(id(b.first))
+print(a.add())
+print()
+
+print("사칙연산 계산기 테스트2")
+class FourCal:
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result 
+
+a = FourCal()
+a.setdata(90,9)
+print(a.first)
+print(a.add())
+print(a.mul())
+print(a.sub())
+print(a.div())
